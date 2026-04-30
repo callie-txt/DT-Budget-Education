5def2b57dc1e93b858558f046adea5bb0c907446
callie-txtgit config --global user.email crln.txt@gmail.com <crln.txt@gmail.comgit config --global user.name callie-txt>
2026-04-30 15:19:43 +1200

    Homepage - Bg and Titel

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app= Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/information')
+def information():
+    return render_template('info.html')
+
+@app.route('/quiz')
+def quiz():
+    return render_template('quiz.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
