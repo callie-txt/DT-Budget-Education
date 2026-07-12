36a8341d8cfd0a9c4d12470cf9a98a9a81e140db
callie-txtgit config --global user.email crln.txt@gmail.com <crln.txt@gmail.comgit config --global user.name callie-txt>
2026-07-12 20:57:32 +1200

    changed goal js into python

--- main.py
+++ main.py
@@ -10,9 +10,38 @@ def home():
 def information():
     return render_template('info.html')
 
-@app.route('/goal')
+@app.route('/goal', methods=['GET', 'POST'])
 def goal():
-    return render_template('goal.html')
+    goal = ''
+    amount = ''
+    weeks = ''
+
+    if request.method == 'POST':
+        goal_text = request.form.get('goal', '').strip()
+        amount_text = request.form.get('amount', '').strip()
+        weeks_text = request.form.get('weeks', '').strip()
+
+        goal = int(goal_text) if goal_text else ''
+        amount = int(amount_text) if amount_text else ''
+        weeks = int(weeks_text) if weeks_text else ''
+
+        if goal == '' and amount != '' and weeks != '':
+            goal = amount * weeks
+        elif amount == '' and goal != '' and weeks != '':
+            amount = goal // weeks
+        elif weeks == '' and goal != '' and amount != '':
+            weeks = goal // amount
+        else:
+            goal = ''
+            amount = ''
+            weeks = ''
+
+    return render_template(
+        'goal.html',
+        goal=goal,
+        amount=amount,
+        weeks=weeks,
+    )
 
 @app.route('/income')
 def income():
